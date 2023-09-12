7004aa8b198dc52fcddd2518843e3d45c279777e
Pavel-K-ua <pavel.kolosyuk@gmail.com>
2023-09-12 21:51:28 +0300

    fix

--- src/components/Feedback/Feedback.styled.js
+++
@@ -1,6 +0,0 @@
-// import { styled } from 'styled-components';
-// export const StyledFriendList = styled.ul`
-//   width: 280px;
-//   margin: 0 auto;
-//   margin-bottom: 50px;
-// `;
--- src/components/Statistics/Statistics.styled.js
+++
@@ -1,5 +0,0 @@
-// export const StyledFriendList = styled.ul`
-//   width: 280px;
-//   margin: 0 auto;
-//   margin-bottom: 50px;
-// `;
