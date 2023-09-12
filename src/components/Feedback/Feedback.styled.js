c8969d9b363b5c7e7b075eb4b5498a383b036876
Pavel-K-ua <pavel.kolosyuk@gmail.com>
2023-09-12 21:49:29 +0300

    fix

--- src/components/Feedback/Feedback.styled.js
+++ src/components/Feedback/Feedback.styled.js
@@ -1,4 +1,4 @@
-import { styled } from 'styled-components';
+// import { styled } from 'styled-components';
 // export const StyledFriendList = styled.ul`
 //   width: 280px;
 //   margin: 0 auto;
--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,4 +1,3 @@
-import { styled } from 'styled-components';
 // export const StyledFriendList = styled.ul`
 //   width: 280px;
 //   margin: 0 auto;
